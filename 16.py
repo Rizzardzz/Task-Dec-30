cd622e73d0d4836a56a1a857f4345afb6122967b
Rizzardzz <142134800+Rizzardzz@users.noreply.github.com>
2024-12-30 09:12:16 +0545

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,7 @@
+
+a=input('Enter your username:')
+b=input('Enter your password:')
+if a=='rizzardzz'and b=='srijj':
+    print('Username and password valid')
+else:
+    print('invalid username and password')
